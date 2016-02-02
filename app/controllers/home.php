50ffc6aa8da564da040eacd69147744da667b7bf
Elton Morais <1111407@isep.ipp.pt>
2016-02-02 18:33:31 +0000

    Second Commit
    Some updates

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,6 +4,24 @@ class Home extends Controller
 {
 	public function index($name = '')
 	{
-		$this->view('home/index', ['name' => $name]);
+		$this->view('home/index', ['name' => $name]);	
+	}
+
+	public function listAll()
+	{
+		$this->view('home/list');
+	}
+
+	public function add()
+	{
+		$db = $this->model('sqlite');
+
+	   	if(!$db){
+	    	echo $db->lastErrorMsg();
+	   	} else {
+	      	echo "Opened database successfully\n";
+	   	}
+
+		$this->view('home/add');
 	}
 }
---
+++ app/models/sqlite.php
@@ -0,0 +1,28 @@
+<?php 
+class sqlite extends SQLite3
+{
+
+	function __construct()
+  	{	
+    	$this->open('test.db');
+  	}
+
+  	public function createChallengeTable($db)
+  	{
+  		$sql ='
+	      	CREATE TABLE COMPANY
+	    	(ID INT PRIMARY KEY     NOT NULL,
+	     	NAME           TEXT    NOT NULL,
+		    AGE            INT     NOT NULL,
+		    ADDRESS        CHAR(50),
+		    SALARY         REAL)';
+
+   		$return = $db->exec($sql);
+   		if(!$return){
+      		echo $db->lastErrorMsg();
+   		} else {
+      		echo "Table created successfully\n";
+   		}
+   		$db->close();
+  	}
+}
