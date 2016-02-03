db7bfb13001eea55efed97e58fe221d8042d76d9
Elton Morais <1111407@isep.ipp.pt>
2016-02-03 14:54:53 +0000

    Fifth Commit
    Completed the add action with its validation and the list action

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -17,7 +17,7 @@ class Home extends Controller
 		$sql = 'SELECT EMAIL, NAME FROM clients;';
 		$return = $db->query($sql);
 		while ($row = $return->fetchArray()) {
-		    $dataArray.push($row["EMAIL"], $row["NAME"]);
+		    $dataArray[] = [$row["EMAIL"], $row["NAME"]];
 		}
 		$db->close();
 
@@ -45,12 +45,28 @@ class Home extends Controller
 	{
 		// create new sqlite object that will open or create a new db called challenge
 		$db = $this->model('sqlite');
-		
-		// if table clients doesn't exist than create it 
-		$sql = 'INSERT INTO clients (EMAIL, NAME) VALUES ("' . $email . '","' . $username . '");';
-		$return = $db->exec($sql);
-		$db->close();
 
-		echo json_encode(array('status' => 'success'));
+		// escape values to prevent SQL injection
+		$email = $db->escapeString($email);
+		$username = $db->escapeString($username);
+		
+		// check if email already exists
+		$sql = 'SELECT count(*) as total FROM clients WHERE EMAIL = "' . $email . '";';
+		$result = $db->querySingle($sql);
+
+		// if email exists than return error
+		if ($result != 0) {
+			// build error array
+			$arrayToReturn = array('status' => 'error', 'message' => 'Email already exists.');
+		} else {
+			// if email doesn't exists than insert it
+			$sql = 'INSERT INTO clients (EMAIL, NAME) VALUES ("' . $email . '","' . $username . '");';
+			$db->exec($sql);
+			
+			// build success array
+			$arrayToReturn = array('status' => 'success');
+		}
+		$db->close();
+		echo json_encode($arrayToReturn);
 	}
 }
